a8ee7e3666f1bb79f9cbabb5aa7464601ce0a5ca
Zach Young <zachjyoung@gmail.com>
2013-06-20 15:27:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blergh::Application.config.secret_token = '80d1471da1d5840673f3d22ce8e0cccf77ca1ce8d7b8f54ac48dba423d0a22b2dbe5ebcdb02ec9e606aeaae9ea685187c4159b0caaad8354221b0ca3feeb14fb'
